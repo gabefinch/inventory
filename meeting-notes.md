e39a0a3e3c14cc6fba263012071e1f7f07336371
Gabe Finch <gabefinch@gmail.com>
2015-04-24 16:30:03 -0700

    Add test coverage for category#top_level .children .parent .descendents .self_and_descendents

--- meeting-notes.md
+++ meeting-notes.md
@@ -1,6 +1,4 @@
 #Notes from meeting w/ Jane and Barbara on 4/22/2015
-
-
 - Thursday: Meal Planning (in what sense?)
 - Friday: Pickup (Driver and Gleaner(s))
 	Oregon Food Bank ~15% of total
@@ -31,5 +29,17 @@
 - Inventory will justify freezer request etc.
 - Health Inspector requirements(can the inventory app help?)
 
-
 GF 4/22
+
+#Site visit: Goose Hollow Kitchen
+- Storage areas blend into each other, sometimes things are stored in non-conventional area.
+- Freezers are marked, shelving is unique for each one.
+- Mixed boxes are the norm, a box of 'pork' could mean a box with several pork items plus some bacon and big slab of beef etc.
+- Super nice folks.
+- Storage is largely in 5(or more?) rooms/spaces
+- Each space has a mix of shelves, cupboards, freezers, fridges etc.
+- Adopting their language for locations to some extent
+- Seems like sorting is half ass, until the menu is planned and then boxes are sorted to meet the demands of the menu.
+- How to incentiveise early sorting. How to make resorting after the weekend happen?
+- Lots of potential for inventory to walk off and very little motivation to change that.
+- Does each level of the tree have an "all of the above"/"misc. protien"/"fridge somewhere"/"room somewhere"?
--- rails/spec/models/category_spec.rb
+++ rails/spec/models/category_spec.rb
@@ -5,4 +5,80 @@ describe Category do
   it { should belong_to :parent }
   it { should have_many :children }
   it { should validate_presence_of :name }
+  describe('.top_level') do
+    it('returns array of all parentless categories') do
+      vegetables = Category.create(name: 'Vegetable')
+      proteins = Category.create(name: 'Proteins')
+      meat = Category.create(name: 'Meat', parent_id: proteins.id)
+      expect(Category.top_level).to eq([vegetables, proteins])
+    end
+  end
+  describe('#descendents') do
+    it('returns descendents of a category') do
+      proteins = Category.create(name: 'Proteins')
+      meat = Category.create(name: 'Meat', parent_id: proteins.id)
+      chicken = Category.create(name: 'Chicken', parent_id: meat.id)
+      expect(proteins.descendents).to eq([meat, chicken])
+    end
+    it('returns empty array for childless') do
+      proteins = Category.create(name: 'Proteins')
+      expect(proteins.descendents).to eq([])
+    end
+  end
+  describe('#self_and_descendents') do
+    it('returns self and descendents of a category') do
+      proteins = Category.create(name: 'Proteins')
+      meat = Category.create(name: 'Meat', parent_id: proteins.id)
+      chicken = Category.create(name: 'Chicken', parent_id: meat.id)
+      expect(proteins.self_and_descendents).to eq([proteins, meat, chicken])
+    end
+    it('returns array of self only for a childless category') do
+      proteins = Category.create(name: 'Proteins')
+      expect(proteins.descendents).to eq([])
+    end
+  end
+  describe('#children') do
+    it('returns children of a category') do
+      proteins = Category.create(name: 'Proteins')
+      meat = Category.create(name: 'Meat', parent_id: proteins.id)
+      expect(proteins.children).to eq([meat])
+    end
+    it('returns empty array for childless') do
+      proteins = Category.create(name: 'Proteins')
+      expect(proteins.children).to eq([])
+    end
+  end
+  describe('#children?') do
+    it('returns true for category w/ children') do
+      proteins = Category.create(name: 'Proteins')
+      meat = Category.create(name: 'Meat', parent_id: proteins.id)
+      expect(proteins.children?).to eq(true)
+    end
+    it('returns false for childless category') do
+      proteins = Category.create(name: 'Proteins')
+      expect(proteins.children?).to eq(false)
+    end
+  end
+  describe('#parent') do
+    it('returns parent for category w/ parent') do
+      proteins = Category.create(name: 'Proteins')
+      meat = Category.create(name: 'Meat', parent_id: proteins.id)
+      expect(meat.parent).to eq(proteins)
+    end
+    it('returns nil for parentless category') do
+      proteins = Category.create(name: 'Proteins')
+      expect(proteins.parent).to eq(nil)
+    end
+  end
+  describe('#parent?') do
+    it('returns true for category w/ parent') do
+      proteins = Category.create(name: 'Proteins')
+      meat = Category.create(name: 'Meat', parent_id: proteins.id)
+      expect(meat.parent?).to eq(true)
+    end
+    it('returns false for parentless category') do
+      proteins = Category.create(name: 'Proteins')
+      expect(proteins.parent?).to eq(false)
+    end
+  end
 end
