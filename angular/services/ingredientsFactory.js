bd56ecb703df31c1af5a796f081883c7fa629472
Lizzie <lizziefkoehler@gmail.com>
2015-05-01 16:28:09 -0700

    add ingredients factory

---
+++ angular/services/ingredientsFactory.js
@@ -0,0 +1,9 @@
+potluck.factory('IngredientsFactory', function($http, $cacheFactory){
+	var factory = {};
+	var cache = $cacheFactory('potluck');
+	factory.reloadCache =
+
+	
+
+	return factory;
+});
