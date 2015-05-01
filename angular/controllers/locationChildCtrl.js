99df8c48a5dfb5b02b5c56c018fd052b08965b47
Lizzie <lizziefkoehler@gmail.com>
2015-05-01 12:13:43 -0700

    adding down the line for locations

---
+++ angular/controllers/locationChildCtrl.js
@@ -0,0 +1,11 @@
+potluck.controller('LocationChildCtrl',
+	function($scope, $cacheFactory,$stateParams, UtilitiesFactory){
+
+		var cache = $cacheFactory.get('potluck');
+		$scope.categoryNode = UtilitiesFactory.findById(cache.get('categories'), $stateParams.categoryId);
+		$scope.locationNode = UtilitiesFactory.findById(cache.get('locations'), $stateParams.locationId);
+		
+		$scope.childNodes = locationNode.children;
+		console.log($scope.childNodes);
+
+	});
