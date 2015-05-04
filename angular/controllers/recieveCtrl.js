0c5cfae3152f00eb6daa81429cdbb7f0fca05711
Gabe Finch <gabefinch@gmail.com>
2015-05-04 12:52:46 -0700

    adds queue count badge to 'put-away items' on recieve view

--- angular/controllers/recieveCtrl.js
+++ angular/controllers/recieveCtrl.js
@@ -1,6 +1,18 @@
 potluck.controller('RecieveCtrl',
-	function($scope, $cacheFactory, HomeFactory){
+	function($scope, $cacheFactory){
 
+    var cache = $cacheFactory.get('potluck');
 
+		var ingredients = cache.get('ingredients');
 
-	});
+    $scope.queueCount = function() {
+
+			var count = 0;
+			for (var i=0; i<=ingredients.length-1; i++) {
+				if (ingredients[i].location_id == null) {count += 1;}
+			}
+			console.log(count);
+			return count;
+		};
+
+});
