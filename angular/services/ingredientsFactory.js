faa28d258a26d7eeb9869d0499da99c3fe419015
Gabe Finch <gabefinch@gmail.com>
2015-05-13 15:19:35 -0700

    rearrange ingredientsController based on johnpapa guidelines

--- angular/services/ingredientsFactory.js
+++ angular/services/ingredientsFactory.js
@@ -1,63 +1,72 @@
-potluck.factory('IngredientsFactory',
-  ['$http','$cacheFactory', '$q', 'UtilitiesFactory',
-    function($http, $cacheFactory, $q, UtilitiesFactory){
-      var factory = {};
-      var cache = $cacheFactory.get('potluck');
+;(function() {
+    'use strict';
 
-      factory.postIngredient = function(category_id, location_id){
-        var defer = $q.defer();
-        $http.post('http://localhost:3000/api/ingredients', {
-          "category_id": category_id,
-           "location_id": location_id
-        }).
-        success(function(data) {
-          var ingredients = cache.get('ingredients');
-          ingredients.push(data);
-          cache.put('ingredients',ingredients);
-          defer.resolve(data);
-        }).
-        error(function(status) {
-          console.log('エラー');
-          console.log(status);
-          defer.reject(status);
-        });
-        return defer.promise;
-      };
+    angular
+      .module('potluck')
+      .factory('IngredientsFactory', IngredientsFactory);
 
-      factory.patchIngredient = function(ingredient){
-        $http.patch(
-          'http://localhost:3000/api/ingredients/' + ingredient.id,
-           {"ingredient": ingredient})
-          .success(function() {
-            var ingredients = cache.get('ingredients');
-            var foundIng = UtilitiesFactory.findByIdArray(ingredients, ingredient.id);
-            foundIng.location_id = ingredient.location_id;
-            foundIng.category_id = ingredient.category_id;
-            cache.put('ingredients', ingredients);
-        }).
-        error(function(status) {
-          console.log('エラー');
-          console.log(status);
-        });
-      };
+    IngredientsFactory.$inject = ['$http','$cacheFactory', '$q', 'UtilitiesFactory'];
 
-      factory.removeIngredient = function(ingredient){
-        $http.delete(
-          'http://localhost:3000/api/ingredients/' + ingredient.id,
-           {"ingredient": ingredient})
-          .success(function() {
-            var ingredients = cache.get('ingredients');
-            for(var i = 0; i < ingredients.length; i++) {
-              if(ingredients[i].id == ingredient.id){ingredients.splice(i,1);}
-            }
-            cache.put('ingredients', ingredients);
-        }).
-        error(function(status) {
-          console.log('エラー');
-          console.log(status);
-        });
-      };
+    function IngredientsFactory($http, $cacheFactory, $q, UtilitiesFactory){
+        var cache = $cacheFactory.get('potluck');
 
-      return factory;
-  }]
-);
+        var factory = {
+          ingredients: ingredients
+          find: find,
+          postIngredient: postIngredient,
+          patchIngredient: patchIngredient,
+          removeIngredient: removeIngredient
+        };
+        
+        return factory;
+
+        var ingredients = cache.get('ingredients');
+
+        function find(id) {
+          for (var i = 0; i < ingredients.length; i++) {
+            if (ingredients[i].id == id)
+              {return ingredients[i];}
+          }
+          return null;
+        }
+        function postIngredient(category_id, location_id){
+          // make arg for this an ingredient object
+          var defer = $q.defer();
+          $http.post('http://localhost:3000/api/ingredients', {"category_id": category_id, "location_id": location_id})
+            .success(function(data) {
+              ingredients.push(data);
+              // is cache.put necessary?
+              cache.put('ingredients',ingredients);
+              defer.resolve(data);
+            })
+            .error(function(status) {
+              console.log('エラー: ' + status);
+              defer.reject(status);
+            });
+          return defer.promise;
+        }
+        function patchIngredient(ingredient){
+          $http.patch( 'http://localhost:3000/api/ingredients/' + ingredient.id, {"ingredient": ingredient})
+            .success(function() {
+              // reference local findByIdArray?
+              var foundIng = UtilitiesFactory.findByIdArray(ingredients, ingredient.id);
+              foundIng.location_id = ingredient.location_id;
+              foundIng.category_id = ingredient.category_id;
+              cache.put('ingredients', ingredients);
+            })
+            .error(function(status) {console.log('エラー: ' + status);});
+        }
+        function removeIngredient(ingredient){
+          $http.delete('http://localhost:3000/api/ingredients/' + ingredient.id, {"ingredient": ingredient})
+            .success(function() {
+              for(var i = 0; i < ingredients.length; i++) {
+                if(ingredients[i].id == ingredient.id) {ingredients.splice(i,1);}
+              }
+              cache.put('ingredients', ingredients);
+            })
+            .error(function(status) {console.log('エラー: ' + status);});
+        }
+
+    }
+
+})();
