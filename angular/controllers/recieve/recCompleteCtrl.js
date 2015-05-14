b2ffff099a199d8ec6fae622f02bd4e414c9230b
Lizzie <lizziefkoehler@gmail.com>
2015-05-14 14:46:30 -0700

    update styling and add complete view

---
+++ angular/controllers/recieve/recCompleteCtrl.js
@@ -0,0 +1,55 @@
+potluck.controller('RecCompleteCtrl',[
+  '$scope',
+  '$cacheFactory',
+  '$state',
+  '$stateParams',
+  'UtilitiesFactory',
+  'IngredientsFactory',
+  function($scope, $cacheFactory, $state, $stateParams, UtilitiesFactory, IngredientsFactory){
+    var cache = $cacheFactory.get('potluck');
+    $scope.ingredient = UtilitiesFactory.findByIdArray(cache.get('ingredients'), $stateParams.ingredientId);
+    $scope.ingredients = cache.get('ingredients');
+    var catIdToCat = function(category_id){
+      return UtilitiesFactory.findById(cache.get('categories'),category_id);
+    };
+    $scope.categoryName = function(ingredient){
+      var category = catIdToCat(ingredient.category_id);
+      return category.name;
+    };
+    $scope.recievedMessage = function(ingredient){
+      var created_at = new Date(ingredient.created_at);
+      return "Arrived " + created_at.toLocaleDateString();
+    };
+    $scope.locationMessage = function(ingredient){
+      var location = UtilitiesFactory.findById(
+        cache.get('locations'),
+        ingredient.location_id);
+      if (location == null) {return "Location unknown";}
+      else {return location.name;}
+    };
+    $scope.expireMessage = function(ingredient) {
+      var lifespan = catIdToCat(ingredient.category_id).lifespan;
+      var shelftime =
+      Math.floor((new Date().getTime() - ingredient.created_at)
+      /(24 * 60 * 60 * 1000));
+      if (shelftime > lifespan){return "Expired " + Math.abs(lifespan - shelftime) + " days ago";}
+      else if (shelftime == lifespan){return "Expires today";}
+      else if (shelftime < lifespan){return "Expires in " + (lifespan - shelftime) + " days.";}
+    };
+
+    $scope.use = function(ingredient){
+      IngredientsFactory.removeIngredient(ingredient);
+      $state.go('prep');
+    };
+
+    $scope.toBasket = function(ingredient){
+      ingredient.basketed = true;
+      $state.go('prep');
+    };
+
+    $scope.fromBasket = function(ingredient){
+      ingredient.basketed = null;
+      $state.go('prep');
+    };
+
+}]);
