2f3a01cc8f10d97a318479c9c2327cf1059767e9
Gabe Finch <gabefinch@gmail.com>
2015-05-19 10:37:46 -0700

    Add days_remaining to ingredient model serialization, order by days_remaining on use first view

--- rails/app/serializers/ingredient_serializer.rb
+++ rails/app/serializers/ingredient_serializer.rb
@@ -1,6 +1,10 @@
 class IngredientSerializer < ActiveModel::Serializer
-  attributes :category_id, :location_id, :created_at, :id
+  attributes :category_id, :location_id, :created_at, :id, :days_remaining
 
+
+  def days_remaining
+    object.category.lifespan - ((Time.now - object.created_at)/(60 * 60 * 24)).round
+  end
 
   def created_at
     object.created_at.to_f * 1000
