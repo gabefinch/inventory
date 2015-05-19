62d1f8da7b6d0629c4fadb64bf891be221e81d0f
Gabe Finch <gabefinch@gmail.com>
2015-05-18 17:43:15 -0700

    remove cachefactory and utilitiesfactory injections from prepIng controller and view

--- angular/controllers/prepare/prepIngCtrl.js
+++ angular/controllers/prepare/prepIngCtrl.js
@@ -1,39 +1,32 @@
 potluck.controller('PrepIngCtrl',[
   '$scope',
-  '$cacheFactory',
   '$state',
   '$stateParams',
-  'UtilitiesFactory',
   'IngredientsFactory',
-  function($scope, $cacheFactory, $state, $stateParams, UtilitiesFactory, IngredientsFactory){
-    var cache = $cacheFactory.get('potluck');
-    $scope.ingredient = IngredientsFactory.find($stateParams.ingredientId);
-    $scope.ingredients = cache.get('ingredients');
-    var catIdToCat = function(category_id){
-      return UtilitiesFactory.findById(cache.get('categories'),category_id);
-    };
-    $scope.categoryName = function(ingredient){
-      var category = catIdToCat(ingredient.category_id);
-      return category.name;
-    };
-    $scope.categoryFraction = function(ingredient){
-      var category = catIdToCat(ingredient.category_id);
-      var ings = UtilitiesFactory.findIngsByCat(category);
+  'CategoriesFactory',
+  'LocationsFactory',
+  function($scope, $state, $stateParams, IngredientsFactory, CategoriesFactory, LocationsFactory){
+    var ingredient = IngredientsFactory.find($stateParams.ingredientId);
+    $scope.name = CategoriesFactory.find(ingredient.category_id).name;
+    $scope.categoryFraction = function(){
+      var category = CategoriesFactory.find(ingredient.category_id);
+      var ings = IngredientsFactory.fromCategory(category);
       return " 1 of " + ings.length;
     };
-    $scope.recievedMessage = function(ingredient){
+    $scope.recievedMessage = function(){
       var created_at = new Date(ingredient.created_at);
       return "Arrived " + created_at.toLocaleDateString();
     };
-    $scope.locationMessage = function(ingredient){
-      var location = UtilitiesFactory.findById(
-        cache.get('locations'),
-        ingredient.location_id);
-      if (location == null) {return "Location unknown";}
-      else {return location.name;}
+    $scope.locationMessage = function(){
+      var location = LocationsFactory.find(ingredient.location_id);
+      if (location == null) {
+        return "Location unknown";
+      } else {
+        return location.name;
+      }
     };
-    $scope.expireMessage = function(ingredient) {
-      var lifespan = catIdToCat(ingredient.category_id).lifespan;
+    $scope.expireMessage = function() {
+      var lifespan = CategoriesFactory.find(ingredient.category_id).lifespan;
       var shelftime =
       Math.floor((new Date().getTime() - ingredient.created_at)
       /(24 * 60 * 60 * 1000));
@@ -41,20 +34,22 @@ potluck.controller('PrepIngCtrl',[
       else if (shelftime == lifespan){return "Expires today";}
       else if (shelftime < lifespan){return "Expires in " + (lifespan - shelftime) + " days.";}
     };
-
-    $scope.use = function(ingredient){
+    $scope.use = function(){
       IngredientsFactory.removeIngredient(ingredient);
       $state.go('prep');
     };
-
-    $scope.toBasket = function(ingredient){
+    $scope.toBasket = function(){
       ingredient.basketed = true;
       $state.go('prep');
     };
-
-    $scope.fromBasket = function(ingredient){
+    $scope.fromBasket = function(){
       ingredient.basketed = null;
       $state.go('prep');
     };
-
+    if (ingredient.basketed) {
+      $scope.basketed = true;
+    } else {
+      $scope.basketed = false;
+    }
+    debugger;
 }]);
